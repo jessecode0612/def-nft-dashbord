16b288da2c0856124df98d5c7409a63df132a8e2
unigisper8 <nanjingzhe425@gmail.com>
2022-03-09 15:40:32 -0500

    Seo Update

--- src/components/SEO/index.tsx
+++ src/components/SEO/index.tsx
@@ -1,6 +1,5 @@
 import Head from 'next/head'
 import { useMemo } from 'react'
-import { chainIconUrl, tokenIconUrl } from 'utils'
 
 interface SEOProps {
   cardName?: string
@@ -20,7 +19,7 @@ const SEO = ({ cardName, chain, token, tvl, volumeChange, logo, nftPage = false 
   const isVolumeChangeValid = volumeChange && volumeChange !== 'NaN%' && volumeChange !== 'undefined%'
 
   const cardURL = useMemo(() => {
-    let cardSrc = new URL(`https://og-cards-chi.vercel.app/`)
+    let cardSrc = new URL(`https://dev.getmrblock.com/`)
 
     // If text is default, the image will only have the logo in the center, without any tvl numbers, chain or token name etc
     let text: string = cardName ? (cardName === 'All' ? 'Overall' : cardName) : 'default'
@@ -39,19 +38,8 @@ const SEO = ({ cardName, chain, token, tvl, volumeChange, logo, nftPage = false 
 
     // First url in images should always be the logo of defillama
     let images = nftPage
-      ? [`https://defillama.com/defillama-press-kit/nft/SVG/defillama-nft.svg`]
-      : [`https://defillama.com/defillama-press-kit/defi/SVG/defillama.svg`]
-
-    // chain and token props are used to get logo, if the logo url isn't available in the data of that page
-    if (logo) {
-      images = [...images, logo]
-    } else if (chain && chain !== 'All') {
-      images = [...images, `https://defillama.com${chainIconUrl(chain)}`]
-    } else {
-      if (token && token !== 'All') {
-        images = [...images, `https://defillama.com${tokenIconUrl(token)}`]
-      }
-    }
+      ? [`https://www.mrblocknft.com/assets/images/hero-lg-@2x.jpg`]
+      : [`https://www.mrblocknft.com/assets/images/hero-lg-@2x.jpg`]
 
     for (let image of images) {
       cardSrc.searchParams.append('images', image)
@@ -64,28 +52,28 @@ const SEO = ({ cardName, chain, token, tvl, volumeChange, logo, nftPage = false 
     <Head>
       <meta
         name="description"
-        content="DefiLlama is a DeFi TVL aggregator. It is committed to providing accurate data without ads or sponsored content, as well as transparency."
+        content="MrBlock Defi TVL and NFT volume Trading Tashboard. It is committed to providing accurate data without ads or sponsored content, as well as transparency"
       />
 
-      <meta property="og:title" content="DefiLlama" />
+      <meta property="og:title" content="MrBlock Defi/NFT" />
       <meta property="og:type" content="website" />
       <meta property="og:url" content={windowURL} />
-      <meta property="og:site_name" content="DefiLlama" />
+      <meta property="og:site_name" content="MrBlock Defi/NFT" />
       <meta
         property="og:description"
-        content="DefiLlama is a DeFi TVL aggregator. It is committed to providing accurate data without ads or sponsored content, as well as transparency."
+        content="MrBlock Defi TVL and NFT volume Trading dashboard. It is committed to providing accurate data without ads or sponsored content, as well as transparency."
       />
       <meta property="og:image" content={cardURL} />
 
       <meta name="twitter:card" content="summary_large_image" />
-      <meta property="twitter:domain" content="defillama.com" />
+      <meta property="twitter:domain" content="defi.getmrblock.com" />
       <meta property="twitter:url" content={windowURL} />
-      <meta name="twitter:title" content="DefiLlama" />
-      <meta name="twitter:site" content="@DefiLlama" />
-      <meta name="twitter:creator" content="@DefiLlama" />
+      <meta name="twitter:title" content="MrBlock" />
+      <meta name="twitter:site" content="@MrBlock" />
+      <meta name="twitter:creator" content="@MrBlock" />
       <meta
         name="twitter:description"
-        content="DefiLlama is a DeFi TVL aggregator. It is committed to providing accurate data without ads or sponsored content, as well as transparency."
+        content="MrBlock Defi TVL and NFT volume Trading dashboard. It is committed to providing accurate data without ads or sponsored content, as well as transparency"
       />
       <meta name="twitter:image" content={cardURL} />
     </Head>
